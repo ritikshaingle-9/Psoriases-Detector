81284050320391ae65dbb0c66f78cd2c52e509a8
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-02-21 22:01:05 +0530

    home page added

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-{/*44f75821f5b433f0de19a4ef400268f3*/}
+import logo from './../../assets/logo.png';
+
 export default function Navbar() {
   return (
-    <div className='w-full h-[70px] bg-amber-400'>
-     
-    </div>
+    <div className='w-full h-[75px] bg-[#FFCFCF] p-3 flex'>
+     <div className='ml-2 mt-[-23px]'>
+     <img src={logo} className='h-26'/>
+     </div>
+
+     <div className='flex justify-end mr-46 ml-220 mt-3'>
+      <button className='mr-6  bg-[#E50046] p-2 rounded-lg h-10 text-white'>Diagnosis</button>
+      <button className='mr-6  bg-[#E50046]  p-2 rounded-lg h-10 text-white'>Sign in</button>
+      <button className='mr-6  bg-[#E50046] p-2 rounded-lg h-10 text-white'>Register</button>
+     </div>
+    </div> 
   )
 }
 
