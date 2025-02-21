d711aa2d372fd93b5a1f0afe6703eddf5371a0d5
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-02-21 21:17:21 +0530

    routing-setup

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+{/*44f75821f5b433f0de19a4ef400268f3*/}
+export default function Navbar() {
+  return (
+    <div className='w-full h-[70px] bg-amber-400'>
+     
+    </div>
+  )
+}
+
+ 
